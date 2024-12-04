4abcecbce3877b9638a4292fd53377ea00f84624
Lactorias <samuelsadek11@gmail.com>
2024-12-04 17:29:56 +1100

    tidy

--- make_compile_comands.py
+++ make_compile_comands.py
@@ -25,15 +25,4 @@ if __name__ == "__main__":
         print(json.dumps(commands, indent = 4))
         json.dump(commands, f, indent=4)
     
-    # bros hacking my project
-    # you will be paid $9.99 an hour until this project is done.
-    # lol :X
-    # writin:wg the compitle commands is too much effort,
-    # esp to change it with a new file so.... just write a script lol XD
-    # anyways, this is approx ready i think
-
-    # okie dokie, TY
-    # food time.
-
-    # oki, i done :)
 
